643992d309e6b220396f7495a8a3577612261201
Shekhar Chauhan <94707859+shekhar2922@users.noreply.github.com>
2022-08-30 11:12:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import App2 from './App2';
-import App3 from './App3';
-import App4 from './App4';
-import ReduxApp from './ReduxApp';
 import CartApp from './CartApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
